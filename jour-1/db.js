ee8f3667b98d2d4057715cc6fc53076a4b8095ed
unknown <hassandahab11@gmail.com>
2021-03-09 10:33:17 +0100

    primary day in DB

---
+++ jour-1/db.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb://localhost:27017/garage",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("DB is connected !!");
+  }
+);
+
+const charactersSchema = new mongoose.Schema({
+  marque: String,
+  modèle: String,
+  année: Number,
+});
+
+const characterModel = mongoose.model("cars", charactersSchema);
+
+// characterModel.deleteMany({}).then(() => {
+//   characterModel.create([
+//     {
+//       marque: "Renault", modèle: "Espace", année: 1999,
+//     },
+//     {
+//       marque: "Renault", modèle: "Scenic", année: 2004,
+//     },
+//     { marque: "Peugeot", modèle: "308", année: 2017 },
+//   ]);
+// });
+
+// characterModel.findById(mongoose.Types.ObjectId("604636a89c9fce33d83ee696")).then((response) => console.log(response));
+
+// characterModel.updateMany(
+//     {
+//         modèle: "Espace"
+//     },{
+//         année: 2000,
+//     }
+   
+// ).then((response) => {
+//     console.log(response);
+// })
+// characterModel.deleteMany({
+ 
+//         marque: "Renault"
+//     }
+// ).then(response => {
+//     console.log(response);
+// })
+
+// characterModel.insertMany([ 
+//     {marque:"Aston Martin", modèle:"DB9", année: 2010},
+//     {marque:"Range Rover", modèle: "Discovery Sport", année: 2017}])  
+    
+    // characterModel.find({
+    //     année: {
+    //         $gt: 2005
+    //     }
+    // }).then((response) => console.log(response)); 
+    
+    characterModel.find({ modèle: /o/
+    } ).then((response) => console.log(response)); 
